c37efc869b727ff006906adc1f09c7ba8ea47554
Shay Benarush <156650984+ShayBenarush@users.noreply.github.com>
2026-05-30 19:16:15 +0300

    JavaDoc has been added
    Add JavaDoc to the file

--- GoNaturePrototype/src/server/control/OrderController.java
+++ GoNaturePrototype/src/server/control/OrderController.java
@@ -1,20 +1,45 @@
 package server.control;
 
 import common.dto.OrderDTO;
-import server.dao.OrderDAO;
 
+/**
+ * Handles order-related operations and communication
+ * between the server and data access layer.
+ */
 public class OrderController {
 
     private OrderDAO dao = new OrderDAO();
 
+    /**
+     * Retrieves an order by its order number.
+     *
+     * @param orderNumber the order number
+     * @return the matching order
+     */
     public OrderDTO getOrder(int orderNumber) {
         return dao.getOrderByNumber(orderNumber);
     }
 
+    /**
+     * Updates an existing order.
+     *
+     * @param orderNumber the order number
+     * @param newDate the updated order date
+     * @param newVisitors the updated number of visitors
+     * @return true if the update succeeded
+     */
     public boolean updateOrder(int orderNumber, String newDate, int newVisitors) {
         return dao.updateOrder(orderNumber, newDate, newVisitors);
     }
 
+    /**
+     * Inserts a new order into the system.
+     *
+     * @param orderDate the order date
+     * @param numberOfVisitors number of visitors in the order
+     * @param subscriberId the subscriber ID
+     * @return the created order
+     */
     public OrderDTO insertOrder(String orderDate, int numberOfVisitors, int subscriberId) {
         return dao.insertOrder(orderDate, numberOfVisitors, subscriberId);
     }
