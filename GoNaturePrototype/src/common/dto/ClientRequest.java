417775f894736f046612760e65715b686f92090c
Shay Benarush <156650984+ShayBenarush@users.noreply.github.com>
2026-06-06 17:52:49 +0300

    Add Javadoc to ClientRequest

--- GoNaturePrototype/src/common/dto/ClientRequest.java
+++ GoNaturePrototype/src/common/dto/ClientRequest.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a request sent from the client to the server.
+ */
 public class ClientRequest implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -11,6 +14,11 @@ public class ClientRequest implements Serializable {
     private Map<String, Object> data;
     private long correlationId = 0L;
 
+    /**
+     * Creates a new request with the given type.
+     *
+     * @param type request type
+     */
     public ClientRequest(RequestType type) {
         this.type = type;
         this.data = new HashMap<>();
@@ -20,10 +28,22 @@ public class ClientRequest implements Serializable {
         return type;
     }
 
+    /**
+     * Adds data to the request payload.
+     *
+     * @param key data key
+     * @param value data value
+     */
     public void put(String key, Object value) {
         data.put(key, value);
     }
 
+    /**
+     * Returns data stored under the given key.
+     *
+     * @param key data key
+     * @return matching value
+     */
     public Object get(String key) {
         return data.get(key);
     }
